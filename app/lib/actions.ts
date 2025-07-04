aeaf4d5d2e0648074673524199da90c5224bbea4
ldias89 <lucasdias@haf.com.br>
2025-07-04 16:53:58 -0300

    feat: adicionando tratamento de erros

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,11 +26,14 @@ export async function createInvoice(formData: FormData){
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        values (${customerId}, ${amountInCents}, ${status}, ${date})
-    `
+    try{
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            values (${customerId}, ${amountInCents}, ${status}, ${date})
+        `
+    }catch(e){
+        console.log(e);
+    }
     revalidatePath('/dashboard/invoices'); //limpa o cache para exibir os novos dados na pagina
     redirect('/dashboard/invoices'); //utilizado para redirecionar para a dashboard apos criar a nova fatura
 }
@@ -43,12 +46,15 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+ try{
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+ }catch(e){
+    console.log(e);
+ }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
